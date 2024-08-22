870b102c9b5a316d17cc12f02e2caf3c8adee6d6
GildedDreams <168908685+GildedDreams@users.noreply.github.com>
2024-08-22 14:11:13 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 void print_array(int array[]){
     for (int i=0;i<9;i++){
         printf("%d ",array[i]);
-        if(i!=0 && (i+1)%3==0 && i!=9){
+        if(i!=0 && (i+1)%3==0){
             printf("\n");
         }
     }
