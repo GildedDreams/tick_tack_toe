4e93fe25c0fa248025b8680ee16744156ea908a3
GildedDreams <168908685+GildedDreams@users.noreply.github.com>
2024-08-22 14:10:29 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
-#include<stdlib.h> 
-void accept_ip(int grid[],int input){
-    for (int i=0;i<10;i++){
-        while (i==input){
-            if(grid[i]=1){
-                continue;
-            }else{
-                grid[i]=1
-            }             
-            
+#include<stdlib.h>
+#define grid_size 10 
+
+void print_array(int array[]){
+    for (int i=0;i<9;i++){
+        printf("%d ",array[i]);
+        if(i!=0 && (i+1)%3==0 && i!=9){
+            printf("\n");
         }
     }
+}
+
+void accept_ip(int grid[],int input){
+    if (grid[input] == 1) {
+        printf("Position already filled.\n");
+    } else {
+        grid[input] = 1;
+}
 }
 int main(){
     int grid[10];
     int input;
-    printf("enter a value in one of the following positions-")
-    for (int i=1;i<10;i++)
+    for (int i=0;i<9;i++){
+        grid[i]=99;
+    }
+    printf("enter a value in one of the following positions-\n");
+    for (int i=1;i<grid_size;i++)
     {
         printf("%d ",i);
         if (i%3==0)
@@ -24,6 +33,9 @@ int main(){
             printf("\n");
         }
     }
+    printf("-->");
     scanf("%d",&input);
+    input -=1 ;// we use this line because array starts from 0 but user inputs are from 1 to 9
     accept_ip(grid,input);
+    print_array(grid);
 }
